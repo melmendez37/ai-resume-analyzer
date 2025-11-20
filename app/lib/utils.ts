2ed0369707118af9ca15776708e595ae2cb09c07
Mel Angelo Mendez <mel.mendez37@gmail.com>
2025-11-20 16:27:39 +0800

    Cloud storage, PDF-Image conversion, Key-Value storage, and AI feedback

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -17,3 +17,5 @@ export function formatSize(bytes: number): string {
 
   return (bytes / Math.pow(k, i)).toFixed(2) + " " + sizes[i];
 }
+
+export const generateUUID = () => crypto.randomUUID();
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,14 +1,68 @@
+import { AIResponseFormat } from 'constants';
+import { prepareInstructions } from 'constants';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
 import FileUploader from '~/components/FileUploader';
 import NavBar from "~/components/NavBar";
+import { convertPdfToImage } from '~/lib/pdf2img';
+import { usePuterStore } from '~/lib/puter';
+import { generateUUID } from '~/lib/utils';
 
 const Upload = () => {
+    const { auth, fs, isLoading, ai, kv } = usePuterStore();
+    const navigate = useNavigate();
     const [ isProcessing, setIsProcessing ] = useState(false);
     const [ statusText, setStatusText ] = useState('');
     const [file, setFile] = useState<File | null>(null);
 
     const handleFileSelect = (file: File | null) => {
         setFile(file);
+    }
+
+    const handleAnalyze = async ({ companyName, jobTitle, jobDescription, file }: { companyName: string, jobTitle: string, jobDescription: string, file: File}) => {
+        setIsProcessing(true);
+
+        setStatusText('Uploading your resume...');
+        const uploadedFile = await fs.upload([file]);
+        if(!uploadedFile) return setStatusText('Error: Failed to upload file.');
+
+        setStatusText('Converting to image...');
+        const imageFile = await convertPdfToImage(file);
+        if(!imageFile.file) return setStatusText('Error: Failed to convert PDF to image.');
+
+        setStatusText('Uploading image...');
+        const uploadedImage = await fs.upload([imageFile.file]);
+        if(!uploadedImage) return setStatusText('Error: Failed to upload image.');
+
+        setStatusText('Preparing data...');
+        const uuid = generateUUID();
+        const data = {
+            id: uuid,
+            resumePath: uploadedFile.path,
+            companyName,
+            jobTitle,
+            jobDescription,
+            feedback: '',
+        }
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
+
+        setStatusText('Analyzing resume...');
+
+        const feedback = await ai.feedback(
+            uploadedFile.path,
+            prepareInstructions({jobTitle, jobDescription})
+        )
+
+        if(!feedback) return setStatusText('Error: Failed to get feedback from AI.');
+
+        const feedbackText = typeof feedback.message.content === 'string' 
+            ? feedback.message.content : feedback.message.content[0].text;
+
+        data.feedback = JSON.parse(feedbackText);
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
+
+        setStatusText('Analysis complete! Redirecting...');
+        console.log('Feedback:', data.feedback);
     }
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -18,13 +72,14 @@ const Upload = () => {
 
         const formData = new FormData(form);
 
-        const companyName = formData.get('company-name');
-        const jobTitle = formData.get('job-title');
-        const jobDescription = formData.get('job-description');
+        const companyName = formData.get('company-name') as string;
+        const jobTitle = formData.get('job-title') as string;
+        const jobDescription = formData.get('job-description') as string;
 
-        console.log({
-            companyName, jobTitle, jobDescription, file
-        })
+        if(!file) return;
+
+        handleAnalyze({ companyName, jobTitle, jobDescription, file });
+
     }
 
   return (
@@ -36,7 +91,7 @@ const Upload = () => {
                 {isProcessing ? (
                     <>
                         <h2>{statusText}</h2>
-                        <img src="/images/resume-scan.pdf"  className="w-full"/>
+                        <img src="/images/resume-scan.gif"  className="w-full"/>
                     </>
                 ) : (
                     <h2>Drop your resume for an ATS Score and Improvement Tips</h2>
