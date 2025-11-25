156b030307a12a1e1558bc30dc81b4633b5a22b3
Mel Angelo Mendez <mel.mendez37@gmail.com>
2025-11-25 15:25:42 +0800

    Implement wipe functionality; added ATS score + tips

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -4,6 +4,7 @@ import { Link, useNavigate, useParams } from 'react-router';
 import Summary from '~/components/Summary';
 import { usePuterStore } from '~/lib/puter';
 import Details from '~/components/Details';
+import ATS from '~/components/ATS';
 
 export const meta = () => ([
     { title: "Resumeind | Review" },
@@ -93,6 +94,7 @@ const Resume = () => {
                 {feedback ? (
                     <div className='flex flex-col gap-8 animate-in fade-in duration-1000'>
                         <Summary feedback={feedback}/>
+                        <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []}/>
                         <Details feedback={feedback}/>
                     </div>
                 ) : (
